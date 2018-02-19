1e452df68413b0f1d7c18354f3efcf65e89dea94
Honorio Vega <hvega@csumb.edu>
2018-02-18 22:07:15 -0800

    Associated TableView outlet to BusinessViewController

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -12,6 +12,7 @@ class BusinessesViewController: UIViewController {
     
     var businesses: [Business]!
     
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
